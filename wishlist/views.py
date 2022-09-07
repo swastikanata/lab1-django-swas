6097d7c8f2cf273a8cf28f000638fc2522e0ab78
Made Swastika Nata Negara <swastika.nata1804@gmail.com>
2022-09-07 12:34:37 +0700

    Create wishlist app

---
+++ wishlist/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from wishlist.models import ItemWishlist
+
+# Create your views here.
+def show_wishlist(request):
+    data_wishlist_item = ItemWishlist.objects.all()
+    context = {
+        'list_item': data_wishlist_item,
+        'name': 'Swas'
+    }
+    return render(request, "wishlist.html", context)
+
