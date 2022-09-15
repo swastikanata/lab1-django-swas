560fcd04473f9052b4869fe1b0696f7979d3c9da
Made Swastika Nata Negara <swastika.nata1804@gmail.com>
2022-09-15 14:33:21 +0700

    Tutorial 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import ItemWishlist
 
 # Create your views here.
@@ -10,3 +12,18 @@ def show_wishlist(request):
     }
     return render(request, "wishlist.html", context)
 
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
+def show_json_by_id(request, id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
